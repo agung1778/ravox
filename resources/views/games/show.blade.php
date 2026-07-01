1918b7421ee979d22b2347ab911140595689d13f
agung1778 <157602405+agung1778@users.noreply.github.com>
2026-07-01 13:49:32 +0700

    typo

--- resources/views/games/show.blade.php
+++ resources/views/games/show.blade.php
@@ -197,10 +197,10 @@ asset('storage/'.$game->banner))
             Related Games
         </h2>
         <p class="text-orange-500 text-sm">
-            {{ ucfirst($$relatedGames->genre) }}
+            {{ ucfirst($relatedGames->genre) }}
         </p>
         <p class="text-gray-500 text-sm">
-            {{ ucfirst($$relatedGames->engine) }}
+            {{ ucfirst($relatedGames->engine) }}
         </p>
         <div class="grid  sm:grid-cols-2  xl:grid-cols-3  gap-8">
             @foreach($relatedGames as $related)

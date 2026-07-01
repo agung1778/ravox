0ba3de6a4078c560621379ad5d5b2d101d0f169c
agung1778 <157602405+agung1778@users.noreply.github.com>
2026-07-01 13:52:38 +0700

    delete show

--- resources/views/games/show.blade.php
+++ resources/views/games/show.blade.php
@@ -196,12 +196,6 @@ asset('storage/'.$game->banner))
         <h2 class="text-4xl font-bold mb-10">
             Related Games
         </h2>
-        <p class="text-orange-500 text-sm">
-            {{ ucfirst($relatedGames->genre) }}
-        </p>
-        <p class="text-gray-500 text-sm">
-            {{ ucfirst($relatedGames->engine) }}
-        </p>
         <div class="grid  sm:grid-cols-2  xl:grid-cols-3  gap-8">
             @foreach($relatedGames as $related)
             <a href="{{ route('games.show',$related->slug) }}"
